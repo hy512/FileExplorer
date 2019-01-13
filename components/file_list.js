fdb21c87b851f3e6e1e8beaf2ba471a3a458f054
hy <scalability@foxmail.qq.com>
2019-01-13 11:05:22 +0800

    目录浏览功能实现; 多选文件解决中;

--- components/file_list.js
+++ components/file_list.js
@@ -6,10 +6,7 @@ function initVue() {
         data: function () {
             return {
                 path: "",
-                list: [
-                    { name: ".", size: "", modify: "", icon: "fa-folder-o", path: ".", isDirectory: true },
-                    { name: "..", size: "", modify: "", icon: "fa-folder-o", path: "..", isDirectory: true }
-                ]
+                list: []
             }
         },
         mounted: function () {
@@ -74,16 +71,24 @@ function initVue() {
             },
             // 进入一个目录
             intoDirectory: function (entry) {
+                console.log("into: " + entry.fullPath)
                 if (!entry.isDirectory) return;
                 this.path = entry.fullPath;
                 var that = this;
-                console.log("into: " + entry.fullPath)
+                this.list = [];
 
                 // 设置当前目录信息
-                var currentDir = this.list[0];
-                currentDir.isDirectory = entry.isDirectory;
-                currentDir.path = entry.fullPath;
-                this.list.splice(0, 1, currentDir);
+                var currentDir = {
+                    name: ".",
+                    size: "",
+                    modify: "",
+                    icon: "fa-folder-o",
+                    path: entry.fullPath,
+                    isDirectory: entry.isDirectory,
+                    entry: entry
+                };
+                // this.list.splice(0, 1, currentDir);
+                this.list.push(currentDir);
                 entry.getMetadata(function (meta) {
                     var dir = that.list[0];
                     dir.modify = meta.modificationTime.toISOString();
@@ -92,9 +97,15 @@ function initVue() {
 
                 // 获取父目录信息
                 entry.getParent(function (entry) {
-                    var dir = that.list[1];
-                    dir.path = entry.fullPath;
-                    dir.isDirectory = entry.isDirectory;
+                    var dir = {
+                        name: "..",
+                        size: "",
+                        modify: "",
+                        icon: "fa-folder-o",
+                        path: entry.fullPath,
+                        isDirectory: entry.isDirectory,
+                        entry: entry
+                    };
                     that.list.splice(1, 1, dir);
                     entry.getMetadata(function (meta) {
                         var dir = that.list[1];
@@ -107,24 +118,40 @@ function initVue() {
                     .readEntries(function (children) {
                         if (!children.length) return;
                         // 读取所有文件条目
-                        that.list = that.list.slice(0, 2)
-                            .concat(children.map(function (v, i) {
-                                return {
-                                    name: v.name,
-                                    path: v.fullPath,
-                                    isDirectory: v.isDirectory
-                                };
-                            }));
-                        // console.log(JSON.stringify(that.list));
+                        var fileList = children.filter(function(value, index) {
+                            if (value === null || value === undefined) return false;
+                            return true;
+                        }).map(function (v, i) {
+                            return {
+                                name: v.name,
+                                path: v.fullPath,
+                                isDirectory: v.isDirectory,
+                                icon: v.isDirectory ? "fa-folder-o" : "fa-file-o",
+                                entry: v
+                            };
+                        }).sort(function (l, r) {
+                            if (l.isDirectory !== r.isDirectory) {
+                                if (l.isDirectory) return -1;
+                                else return 1;
+                            }
+                            var lNameLen = l.name.length;
+                            var rNameLen = r.name.length;
+                            var limitLen = lNameLen > rNameLen ? rNameLen : lNameLen;
+                            for (var i = 0; i < limitLen; i++) {
+                                if (l.name[i] === r.name[i]) continue;
+                                if (l.name[i] > r.name[i]) return 1;
+                                return -1;
+                            }
+                            return lNameLen > rNameLen ? 1 : -1;
+                        });
+                        that.list = that.list.slice(0, 2).concat(fileList);
                         // 读取所有条目详情信息
-                        children.forEach(function (v, i) {
-                            v.getMetadata(function (meta) {
-                                // console.log(JSON.stringify(that.list));
-                                var index = i + 2;
-                                var dir = that.list[index];
-                                // console.log(JSON.stringify(dir))
-                                dir.modify = meta.modificationTime.toISOString();
-                                that.list.splice(index, 1, dir);
+                        fileList.forEach(function (value, index) {
+                            if (!value || !value.entry) return;
+                            value.entry.getMetadata(function (meta) {
+                                value.modify = meta.modificationTime.toISOString();
+                                if (!value.isDirectory) value.size = meta.size;
+                                that.list.splice(index+2, 1, value);
                             }, that.error);
                         });
                     }, that.error);
--- components/file_list_item.js
+++ components/file_list_item.js
@@ -10,12 +10,16 @@ var ListItem = {
         var self = this;
         // 添加长按和点击事件
         hammer.on("press", function () {
-            var checked = !self.checked;
-            self.checked = checked;
-            self.$emit("press", { checked: checked, path: self.path });
         });
         hammer.on("tap", function () {
             self.$emit("tap", { path: self.path });
+        });
+        var multiSelect = new Hammer(this.$refs.multiSelect, {});
+        multiSelect.on("tap", function(event) {
+            var checked = !self.checked;
+            self.checked = checked;
+            self.$emit("xselect", { checked: checked, path: self.path });
+            event.preventDefault();
         });
     },
     computed: {
@@ -31,13 +35,13 @@ var ListItem = {
         '<div class="mui-media-object mui-pull-left">' +
         '<span :class="[\'fa\', icon, \'fa-lg\']"></span>' +
         '</div>' +
-        '<div class="mui-media-object mui-pull-right">' +
+        '<div ref="multiSelect" class="mui-media-object mui-pull-right">' +
         '<span :style="checkStyle" class="fa fa-circle"></span>' +
         '</div>' +
         '<div class="mui-media-body">' +
         '{{name}}' +
         '<div class="mui-row">' +
-        '<div class="mui-col-xs-4" style="padding-left: 10px;">{{modify}}</div>' +
+        '<div class="mui-col-xs-8" style="padding-left: 10px;">{{modify}}</div>' +
         '<div class="mui-col-xs-4" style="padding-left: 10px;">{{size}}</div>' +
         '</div>' +
         // "<p class='mui-ellipsis'></p>" +
